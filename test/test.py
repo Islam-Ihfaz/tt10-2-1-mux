53e94a47bed9f95a8c7bdb5cd8c78e03a42d4acc
Islam-Ihfaz <144639167+Islam-Ihfaz@users.noreply.github.com>
2025-06-26 11:29:05 -0400

    Update test.py

--- test/test.py
+++ test/test.py
@@ -28,7 +28,7 @@ async def test_project(dut):
     # Set the input values you want to test
     dut.ui_in[0].value = 0 # Input A
     dut.ui_in[1].value = 1 # Input B
-    dut.ui_in[1].value = 0 # S (Select line)
+    dut.ui_in[2].value = 0 # S (Select line)
 
     # Wait for some clock cycles to see the output values
     await ClockCycles(dut.clk, 25)
@@ -43,18 +43,18 @@ async def test_project(dut):
     
     dut.ui_in[0].value = 1 # A = 1
     dut.ui_in[1].value = 0 # B = 0
-    dut.ui_in[1].value = 0 # Select = 0
+    dut.ui_in[2].value = 0 # Select = 0
     await ClockCycles(dut.clk, 25)
     assert dut.uo_out[0].value == 1 # S = 0 -> O = A = 1
 
     dut.ui_in[0].value = 1 # A = 1
     dut.ui_in[1].value = 0 # B = 0
-    dut.ui_in[1].value = 1 # Select = 1
+    dut.ui_in[2].value = 1 # Select = 1
     await ClockCycles(dut.clk, 25)
     assert dut.uo_out[0].value == 0 # Output follows B when S = 1
 
     dut.ui_in[0].value = 0 # A = 0
     dut.ui_in[1].value = 1 # B = 1
-    dut.ui_in[1].value = 1 # Select = 1
+    dut.ui_in[2].value = 1 # Select = 1
     await ClockCycles(dut.clk, 25)
     assert dut.uo_out[0].value == 1 # S = 1 -> O = B = 1 
